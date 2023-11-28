adae1f0362b51e95e852fc9d98fc362d6520342f
lulb11 <bordeslucas11@gmail.com>
2023-11-28 16:22:25 +0100

    supr user

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ app.use(express.json());
 const movieControllers = require("./controllers/movieControllers");
 const userControllers = require("./controllers/userControllers");
 
-
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", userControllers.getUsers);
@@ -16,5 +15,7 @@ app.post("/api/movies", movieControllers.postMovie);
 app.post("/api/users", userControllers.postUser);
 app.put("/api/movies/:id", movieControllers.updateMovie);
 app.put("/api/users/:id", userControllers.updateUser);
+app.delete("/api/movies/:id", movieControllers.deleteMovie);
+app.delete("/api/users/:id", userControllers.deleteUser);
 
 module.exports = app;
